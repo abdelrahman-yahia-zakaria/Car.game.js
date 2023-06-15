b43cf6cf6c3f81349e86b25dab25e9dfc7c73b77
moaaz90 <moaaz.yahia.zakaria@gmail.com>
2023-06-15 04:32:30 +0300

    first

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ window.onload = function () {
   var k = 0;
   // ********SIZE.4******** \\
   var N = 200;
-  var F = 0;
+  var F = 500;
   // var container = $("#container");
 
   // ********PNG.1******** \\
   var images1 = new Image();
-  images1.src = "/images/cars1.png";
+  images1.src = "./images/cars1.png";
   // ********PNG.2******** \\
   var images2 = new Image();
-  images2.src = "/images/cars2.png";
+  images2.src = "./images/cars2.png";
   // ********PNG.3******** \\
   var images3 = new Image();
-  images3.src = "/images/cars3.png";
+  images3.src = "./images/cars3.png";
   // ********PNG.4******** \\
   var images4 = new Image();
-  images4.src = "/images/cars4.png";
+  images4.src = "./images/cars4.png";
 
   function draw() {
     // **********1********* \\
@@ -53,10 +53,26 @@ window.onload = function () {
     // **********4********* \\
     context.beginPath();
     context.drawImage(images4, N, F, 60, 120);
-    F -= 20;
-    if (F <= -100) {
-      F = 700;
+    if (left){
+      N -= 20;
+      if (N <= 120) {
+        N = 120;
+      }
+    }
+    if (right){
+      N += 20;
+      if (N >= 400) {
+        N = 400;
+      }
     }
   }
   setInterval(draw, 50);
 };
+function top1() {
+  left = true;
+  right = false;
+}
+function wealth() {
+  right = true;
+  left = false;
+}
