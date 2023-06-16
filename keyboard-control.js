8b0fcd9e55bf93355c82db2b7a9f490901d18471
moaaz90 <moaaz.yahia.zakaria@gmail.com>
2023-06-16 16:15:12 +0300

    first

--- keyboard-control.js
+++ keyboard-control.js
@@ -1,36 +1,39 @@
+var lo = 5;
 window.addEventListener("keydown", e => {
     switch(e.key){
         case "ArrowUp":
-            input.F -= 5;
+            input.F -= lo;
             if (input.F <= -10) {
               input.F = -10;
             }
             break
         case "ArrowDown":
-            input.F += 5;
+            input.F += lo;
             if (input.F >= 510) {
               input.F = 510;
             }
             break
         case "ArrowLeft":
-            input.N -= 5;
+            input.N -= lo;
              if (input.N <= 120) {
                  input.N = 120;
             }
             break
         case "ArrowRight":
-            input.N += 5;
+            input.N += lo;
              if (input.N >= 400) {
                  input.N = 400;
             }
             break
         case "w":
+            if (lo < 10){
+                lo += 1;
+            }
               break
         case "s":
-              input.F += 5;
-              if (input.F >= 510) {
-                input.F = 510;
-              }
+            if (lo > 5){
+                lo -= 1;
+            }
               break
         case "a":
               input.N -= 5;
